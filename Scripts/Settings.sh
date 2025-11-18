9353e9379ec0318e4798e0b71e4dd42c8f65a65e
Puter <472686615@qq.com>
2025-11-18 16:20:09 +0800

    优化部分内容

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -64,3 +64,50 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 		echo "qualcommax set up nowifi successfully!"
 	fi
 fi
+
+
+#设置wifi 将内容写入到文件，该文件在系统第一次启动时执行
+SET_WIFI_FILE="./package/base-files/files/etc/uci-defaults/990_set-wifi-uci.sh"
+cat > "$SET_WIFI_FILE" << 'EOF'
+#!/bin/sh
+
+board_name=$(cat /tmp/sysinfo/board_name)
+configure_wifi() {
+	local radio=$1
+	local channel=$2
+	local htmode=$3
+	local txpower=$4
+	local ssid=$5
+	local key=$6
+	local encryption=${7:-"psk2+ccmp"} # 新增 encryption 参数，如果为空则默认为 psk2+ccmp
+	local now_encryption=$(uci get wireless.default_radio${radio}.encryption)
+	if [ -n "$now_encryption" ] && [ "$now_encryption" != "none" ]; then
+		return 0
+	fi
+	uci -q batch <<EOF_UCI
+set wireless.radio${radio}.channel="${channel}"
+set wireless.radio${radio}.htmode="${htmode}"
+set wireless.radio${radio}.mu_beamformer='1'
+set wireless.radio${radio}.country='US'
+set wireless.radio${radio}.txpower="${txpower}"
+set wireless.radio${radio}.cell_density='0'
+set wireless.radio${radio}.disabled='0'
+set wireless.default_radio${radio}.ssid="${ssid}"
+set wireless.default_radio${radio}.encryption="${encryption}"
+set wireless.default_radio${radio}.key="${key}"
+set wireless.default_radio${radio}.ieee80211k='1'
+set wireless.default_radio${radio}.time_advertisement='2'
+set wireless.default_radio${radio}.time_zone='CST-8'
+set wireless.default_radio${radio}.bss_transition='1'
+set wireless.default_radio${radio}.wnm_sleep_mode='1'
+set wireless.default_radio${radio}.wnm_sleep_mode_no_keys='1'
+EOF_UCI
+}
+
+configure_wifi 0 149 HE80 20 'JDC_AX1800PRO_5G' '12345678'
+configure_wifi 1 1 HE20 20 'JDC_AX1800PRO' '12345678'
+
+uci commit wireless
+/etc/init.d/network restart
+
+EOF
